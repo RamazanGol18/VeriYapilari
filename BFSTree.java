85333fdb37a3eef57e7dd36d3c3ccec90cf3336b
RamazanGol18 <158610558+RamazanGol18@users.noreply.github.com>
2024-02-04 14:30:13 +0300

    Add files via upload

---
+++ BFSTree.java
@@ -0,0 +1,52 @@
+
+package com.mycompany.bfstree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
+
+public class BFSTree {
+
+    public static void main(String[] args) {
+        
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        System.out.println("BFS Sırası:");
+        bfs(root);
+    }
+
+    static void bfs(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            Node currentNode = queue.poll();
+            System.out.print(currentNode.data + " ");
+
+            if (currentNode.left != null) {
+                queue.add(currentNode.left);
+            }
+
+            if (currentNode.right != null) {
+                queue.add(currentNode.right);
+            }
+        }
+    }
+}
